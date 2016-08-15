f4715a50f851af55a956088ab33b1403f67179b0
bj86 <itsbarrylol@gmail.com>
2016-08-15 18:55:10 +0100

    Add files via upload

---
+++ Telefonbok/Telefonbok.py
@@ -0,0 +1,39 @@
+import pickle
+
+def menu():
+    data = pickle.load(open('test.pickle', 'rb'))
+    print("""
+    1. read
+    2. add
+    3. remove""")
+    answer = int(input(''))
+    if answer == 1:
+        return read(data)
+    elif answer == 2:
+        return add(data)
+    elif answer == 3:
+        return remove(data)
+    else:
+        return print('Not a valid option!'), menu()
+
+def load():
+    return data, menu()
+
+def read(data):
+    name = str(input('name: '))
+    print(('number:'), (data[name]))
+    return menu()
+
+def add(data):
+    name = str(input('name: '))
+    number = int(input('number: '))
+    data[str(name)] = number
+    pickle.dump(data, open('test.pickle', 'wb'))
+    return menu()
+
+def remove():
+    name = str(input('name: '))
+    del data[name]
+    return menu()
+
+menu()
